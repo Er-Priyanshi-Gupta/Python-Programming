e4ff3fda634eb97bd3a590376c1f9e1a6efa935e
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-05 16:42:11 +0530

    file updated

---
+++ BasicPrograms/match.py
@@ -0,0 +1,13 @@
+print("Enter a number from 0 to 6")
+number = int(input())
+def weekday(n):
+   match n:
+      case 0: return "Monday"
+      case 1: return "Tuesday"
+      case 2: return "Wednesday"
+      case 3: return "Thursday"
+      case 4: return "Friday"
+      case 5: return "Saturday"
+      case 6: return "Sunday"
+      case _: return "Invalid day number"
+print(weekday(number))
