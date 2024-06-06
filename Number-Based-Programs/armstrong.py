c0f6843eac5ad3840ebdb50602f02f52fc0c26d1
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-06 14:25:50 +0530

    file updated

---
+++ Number-Based-Programs/armstrong.py
@@ -0,0 +1,19 @@
+def armstrong(num):
+    length = len(str(num))
+    sum = 0
+    while num :
+        remainder = num%10
+        sum+= remainder**length
+        num//=10
+    return sum
+def isarmstrong(n):
+    if(n == armstrong(n)):
+        return 1
+    else:
+        return 0
+print("Enter a number to check whether it is Armstrong or not.\n")
+number = int(input())
+if(isarmstrong(number)):
+    print("Number",number, "is Armstrong")
+else:
+    print("Number",number, "is not Armstrong")
