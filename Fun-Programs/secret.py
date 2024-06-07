d11c12bbadd6fea8faf4f3d4177e89363a895bce
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-07 16:12:09 +0530

    file updated

---
+++ Fun-Programs/secret.py
@@ -0,0 +1,14 @@
+import string
+import random
+def reverse(sentence):
+    return sentence[::-1]
+def encode(sentence):
+    if len(sentence)<3:
+        return reverse(sentence)
+    else:
+        pass
+def decode(sentence):
+    if len(sentence)<3:
+        return reverse(sentence)
+    else:
+        pass
