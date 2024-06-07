4ea79ab1a2affb9fb4e076c43595e59715da5f68
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-07 17:23:28 +0530

    file updated

--- Fun-Programs/secret.py
+++ Fun-Programs/secret.py
@@ -4,11 +4,40 @@ def reverse(sentence):
     return sentence[::-1]
 def encode(sentence):
     if len(sentence)<3:
-        return reverse(sentence)
+         print(reverse(sentence))
     else:
-        pass
+        s = list (sentence)
+        l = s[0]
+        s.remove(s[0])
+        s.append(l)
+        s1 = ""
+        s1 = s1.join(s)
+        alphabet = (string.ascii_lowercase + string.ascii_uppercase)
+        start = ''.join((random.choice(alphabet) for i in range(3)))
+        last = ''.join((random.choice(alphabet) for i in range(3)))
+        sentence =  start + s1 + last
+        print(sentence,end=" ")
+        return sentence
 def decode(sentence):
-    if len(sentence)<3:
-        return reverse(sentence)
+    if (len(sentence))<3:
+        print(reverse(sentence))
     else:
-        pass
+        start = sentence[0:3]
+        last = sentence[len(sentence)-3:]
+        sentence = (sentence.replace(last,''))
+        sentence = (sentence.replace(start,''))
+        first = sentence[len(sentence)-1:]
+        sentence = first + sentence
+        sentence = sentence[:len(sentence)-1]
+        print(sentence,end=" ")
+print("Enter a string ")
+userInput = input()
+print("For encoding enter 1 \n For decoding enter 2")
+choice = input()
+if choice == "1":
+    encode(userInput)
+elif choice == '2':
+    decode(userInput)
+else:
+    print("Invalid choice")
+
