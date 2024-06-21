3c15823ae29f909c69919b412ce7e3cf32315201
Sanskar Soni <159034109+Er-Priyanshi-Gupta@users.noreply.github.com>
2024-06-21 14:14:56 +0530

    file updated

--- Utility-Programs/Pronounce.py
+++ Utility-Programs/Pronounce.py
@@ -1,8 +1,8 @@
 import win32com.client 
 speaker = win32com.client.Dispatch("SAPI.SpVoice") 
-while 1: 
-    print("Enter list of people whom you want to give shoutout") 
-    s = input().split(",")
-    print(s)
-    speaker.Speak(s)
+print("Enter list of people whom you want to give shoutout") 
+s = input().split(",")
+print(s)
+for name in s:
+    speaker.Speak(f"Shoutout to{name}")
     
